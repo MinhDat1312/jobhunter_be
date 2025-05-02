a9a1187082012cf9dc0947e23467bdce5cbba4be
MinhDat1312 <nguyenthangdat84@gmail.com>
2025-05-02 22:10:31 +0700

    jobhunter_be

---
+++ src/main/java/vn/minhdat/jobhunter_be/JobhunterBeApplication.java
@@ -0,0 +1,13 @@
+package vn.minhdat.jobhunter_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobhunterBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobhunterBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/minhdat/jobhunter_be/JobhunterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.minhdat.jobhunter_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobhunterBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
