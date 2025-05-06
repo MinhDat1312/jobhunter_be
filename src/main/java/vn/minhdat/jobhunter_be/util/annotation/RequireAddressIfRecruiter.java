c8f4f84b3f1b42b65cc54a68a830a149a19675b5
MinhDat1312 <nguyenthangdat84@gmail.com>
2025-05-06 09:46:38 +0700

    fix annotation to create entity

--- src/main/java/vn/minhdat/jobhunter_be/util/annotation/RequireAddressIfRecruiter.java
+++ src/main/java/vn/minhdat/jobhunter_be/util/annotation/RequireAddressIfRecruiter.java
@@ -1,11 +1,8 @@
 package vn.minhdat.jobhunter_be.util.annotation;
 
 import jakarta.validation.Constraint;
-import jakarta.validation.ConstraintValidator;
-import jakarta.validation.ConstraintValidatorContext;
 import jakarta.validation.Payload;
-import vn.minhdat.jobhunter_be.entity.Recruiter;
-import vn.minhdat.jobhunter_be.entity.User;
+import vn.minhdat.jobhunter_be.util.validation.RequireAddressIfRecruiterValidator;
 
 import java.lang.annotation.*;
 
@@ -19,24 +16,3 @@ public @interface RequireAddressIfRecruiter {
     Class<? extends Payload>[] payload() default {};
 }
 
-class RequireAddressIfRecruiterValidator implements ConstraintValidator<RequireAddressIfRecruiter, User> {
-    @Override
-    public boolean isValid(User o, ConstraintValidatorContext constraintValidatorContext) {
-        if(o instanceof Recruiter recruiter){
-            if (recruiter.getAddress() == null
-                || recruiter.getAddress().getCity().isEmpty() || recruiter.getAddress().getCountry().isEmpty()
-                || recruiter.getAddress().getDistrict().isEmpty() || recruiter.getAddress().getNumber().isEmpty()
-                || recruiter.getAddress().getStreet().isEmpty() || recruiter.getAddress().getWard().isEmpty()
-            ) {
-                constraintValidatorContext.disableDefaultConstraintViolation();
-                constraintValidatorContext
-                        .buildConstraintViolationWithTemplate(
-                                "Recruiter must provide an address or info about address is not empty")
-                        .addPropertyNode("address")
-                        .addConstraintViolation();
-                return false;
-            }
-        }
-        return true;
-    }
-}
---
+++ src/main/java/vn/minhdat/jobhunter_be/util/validation/RequireAddressIfRecruiterValidator.java
@@ -0,0 +1,32 @@
+package vn.minhdat.jobhunter_be.util.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import vn.minhdat.jobhunter_be.entity.Recruiter;
+import vn.minhdat.jobhunter_be.util.annotation.RequireAddressIfRecruiter;
+
+public class RequireAddressIfRecruiterValidator implements ConstraintValidator<RequireAddressIfRecruiter, Object> {
+    public RequireAddressIfRecruiterValidator() {
+    }
+
+    @Override
+    public boolean isValid(Object o, ConstraintValidatorContext constraintValidatorContext) {
+        if(o instanceof Recruiter recruiter){
+            if (recruiter.getAddress() == null
+                    || recruiter.getAddress().getCity().isEmpty() || recruiter.getAddress().getCountry().isEmpty()
+                    || recruiter.getAddress().getDistrict().isEmpty() || recruiter.getAddress().getNumber().isEmpty()
+                    || recruiter.getAddress().getStreet().isEmpty() || recruiter.getAddress().getWard().isEmpty()
+            ) {
+                constraintValidatorContext.disableDefaultConstraintViolation();
+                constraintValidatorContext
+                        .buildConstraintViolationWithTemplate(
+                                "Recruiter must provide an address or info about address is not empty")
+                        .addPropertyNode("address")
+                        .addConstraintViolation();
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
