8b6d8d7e3ed5fa28e799ac079c476aa70282b255
MinhDat1312 <nguyenthangdat84@gmail.com>
2025-06-03 15:42:34 +0700

    swagger

--- src/main/java/vn/minhdat/jobhunter_be/util/FormatResponse.java
+++ src/main/java/vn/minhdat/jobhunter_be/util/FormatResponse.java
@@ -37,6 +37,11 @@ public class FormatResponse implements ResponseBodyAdvice<Object> {
             return body;
         }
 
+        String path = request.getURI().getPath();
+        if(path.startsWith("/v3/api-docs") || path.startsWith("/swagger-ui")){
+            return body;
+        }
+
         if(statusCode >= 400){
             return body;
         } else {
