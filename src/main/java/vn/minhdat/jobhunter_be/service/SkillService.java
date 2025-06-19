b5567faffab3c8c23765ff506f08f2ffee79e709
MinhDat1312 <nguyenthangdat84@gmail.com>
2025-06-19 18:28:16 +0700

    fix pagination

--- src/main/java/vn/minhdat/jobhunter_be/service/SkillService.java
+++ src/main/java/vn/minhdat/jobhunter_be/service/SkillService.java
@@ -55,7 +55,7 @@ public class SkillService {
         Page<Skill> page = this.skillRepository.findAll(spec, pageable);
 
         ResultPaginationResponse.Meta meta = new ResultPaginationResponse.Meta();
-        meta.setPage(pageable.getPageNumber());
+        meta.setPage(pageable.getPageNumber() + 1);
         meta.setPageSize(pageable.getPageSize());
         meta.setPages(page.getTotalPages());
         meta.setTotal(page.getTotalElements());
